c36a50e1bec59d6a88e102a793a2203d97821aa6
Sebastian Fojcik <fojcik.sebastian@gmail.com>
2019-10-24 12:25:24 +0200

    Added Discover Activity

---
+++ app/src/main/java/mwo/lab/tapswap/MyItems.kt
@@ -0,0 +1,12 @@
+package mwo.lab.tapswap
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MyItems : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_items)
+    }
+}
