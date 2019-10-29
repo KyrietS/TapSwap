2c6fd39653496855e2618c2778410cd30a3be439
Kyriet <kyriet.sebastian@gmail.com>
2019-10-29 22:38:38 +0100

    integrated Retrofit with project

--- app/src/main/java/mwo/lab/tapswap/MyItemsActivity.kt
+++ app/src/main/java/mwo/lab/tapswap/MyItemsActivity.kt
@@ -6,18 +6,44 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.widget.Button
 import com.google.gson.Gson
+import mwo.lab.tapswap.api.Endpoints
+import mwo.lab.tapswap.api.APIService
+import mwo.lab.tapswap.api.model.UserItems
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.util.*
 
 class MyItemsActivity : AppCompatActivity() {
 
     private val items = ArrayList<Item>()
 
+    private lateinit var api: Endpoints
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_items)
+
+        // TODO chyba nie wysyła tokenu
+        api = APIService.create()
+
+        val call = api.getUserItems()
+        call.enqueue( object : Callback<UserItems> {
+            override fun onResponse(call: Call<UserItems>, response: Response<UserItems>) {
+
+                Log.d("omg", call.request().headers().toString())
+                val body = response.body()!!
+                Log.d("omg", "RESPONSE")
+                Log.d("omg", body.toString())
+            }
+
+            override fun onFailure(call: Call<UserItems>, t: Throwable) {
+                Log.d("omg", "FAILURE")
+            }
+        })
 
         val recyclerView = findViewById<RecyclerView>( R.id.items_recycler )
 
