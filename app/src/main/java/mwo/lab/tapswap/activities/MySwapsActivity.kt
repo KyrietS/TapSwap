84b1bcc3ad85f0f0035f62b394772f8047e786af
Marta Sokołowska <msokolowska@localhost.localdomain>
2019-11-26 17:45:09 +0100

    add swap screens drafts

--- app/src/main/java/mwo/lab/tapswap/activities/MySwapsActivity.kt
+++ app/src/main/java/mwo/lab/tapswap/activities/MySwapsActivity.kt
@@ -1,4 +1,39 @@
 package mwo.lab.tapswap.activities
 
-class MySwapsActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.Button
+import mwo.lab.tapswap.R
+import mwo.lab.tapswap.adapters.MySwapsAdapter
+
+class MySwapsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_swaps)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.swaps_recycler)
+
+        // for optimisation of recyclerView
+        recyclerView.setHasFixedSize(true)
+
+        // LayoutManager
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // Animator
+        recyclerView.itemAnimator = DefaultItemAnimator()
+
+        val myAdapter = MySwapsAdapter(this)
+        recyclerView.adapter = myAdapter
+
+        val addButton = findViewById<Button>(R.id.addButton)
+        addButton.setOnClickListener {
+            val intent = Intent(this, AddItemActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
