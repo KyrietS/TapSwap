2bf391f3f83225252bc3ca09fa5cb4ced61f64e7
Marta Sokołowska <msokolowska@localhost.localdomain>
2019-11-30 15:10:16 +0100

    start work on swap screens classes

--- app/src/main/java/mwo/lab/tapswap/activities/ConfirmSwapActivity.kt
+++ app/src/main/java/mwo/lab/tapswap/activities/ConfirmSwapActivity.kt
@@ -1,2 +1,12 @@
 package mwo.lab.tapswap.activities
 
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import mwo.lab.tapswap.R
+
+class ConfirmSwapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirm_swap)
+    }
+}
