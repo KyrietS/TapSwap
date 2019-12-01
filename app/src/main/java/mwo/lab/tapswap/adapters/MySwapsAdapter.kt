88f1d6c96e90b24e48759ef0c610b7614aa20eb0
Sebastian Fojcik <fojcik.sebastian@gmail.com>
2019-12-02 00:28:48 +0100

    Filling GUI with pending swaps

--- app/src/main/java/mwo/lab/tapswap/adapters/MySwapsAdapter.kt
+++ app/src/main/java/mwo/lab/tapswap/adapters/MySwapsAdapter.kt
@@ -1,21 +1,14 @@
 package mwo.lab.tapswap.adapters
 
 import android.app.Activity
-import android.app.AlertDialog
-import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
-import android.widget.PopupMenu
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import mwo.lab.tapswap.R
-import mwo.lab.tapswap.activities.ItemPreviewActivity
 import mwo.lab.tapswap.api.APIService
-import mwo.lab.tapswap.api.models.Item
-import mwo.lab.tapswap.api.models.RequestResult
+import mwo.lab.tapswap.api.models.Matches
 import mwo.lab.tapswap.api.models.Swap
 import mwo.lab.tapswap.api.models.UserSwaps
 import mwo.lab.tapswap.views.LoadingView
@@ -27,7 +20,7 @@ class MySwapsAdapter(
     private val context: Activity
 ) : RecyclerView.Adapter<MySwapsAdapter.SwapHolder>() {
 
-    var swaps = listOf<Swap>()
+    var swaps = listOf<Matches.MatchesData.Match>()
 
     override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
         fetchData()
@@ -36,21 +29,21 @@ class MySwapsAdapter(
     fun fetchData() {
         // Sending request for all my swaps
         val api = APIService.create()
-        val call = api.getUserSwaps()
+        val call = api.getPendingMatches()
 
         // Show loading circle
         val loading = context.findViewById<LoadingView>(R.id.loading)!!
         loading.begin()
-        call.enqueue(object : Callback<UserSwaps> {
-            override fun onResponse(call: Call<UserSwaps>, response: Response<UserSwaps>) {
+        call.enqueue(object : Callback<Matches> {
+            override fun onResponse(call: Call<Matches>, response: Response<Matches>) {
                 if (response.isSuccessful) {
-                    swaps = response.body()?.data ?: listOf()
+                    swaps = response.body()?.data?.matches ?: listOf()
                     notifyDataSetChanged()
                     loading.finish()
                 }
             }
 
-            override fun onFailure(call: Call<UserSwaps>, t: Throwable) {
+            override fun onFailure(call: Call<Matches>, t: Throwable) {
                 loading.finish()
                 Toast.makeText(context, "Connection error", Toast.LENGTH_SHORT).show()
             }
@@ -61,10 +54,10 @@ class MySwapsAdapter(
     // every object of this class has it's own reference to the layout element
     // so we call findViewById() only once for each element in the list.
     inner class SwapHolder(swap: View) : RecyclerView.ViewHolder(swap) {
-        var myItemTitle: TextView = swap.findViewById(R.id.my_swap_item_name)
-        var theirItemTitle: TextView = swap.findViewById(R.id.their_swap_item_name)
-        var myItemPhoto: TextView = swap.findViewById(R.id.my_swap_item)
-        var theirItemPhoto: TextView = swap.findViewById(R.id.their_swap_item)
+        var myItemName: TextView = swap.findViewById(R.id.my_item_name)
+        var otherItemName: TextView = swap.findViewById(R.id.other_item_name)
+        var myItemPhoto: ImageView = swap.findViewById(R.id.my_item_photo)
+        var otehrItemPhoto: ImageView = swap.findViewById(R.id.other_item_photo)
         var swapPopupMenu: ImageButton = swap.findViewById(R.id.popup_menu)
     }
 
@@ -84,8 +77,8 @@ class MySwapsAdapter(
     override fun onBindViewHolder(viewHolder: SwapHolder, i: Int) {
         // fill item layout
         val swap = swaps[i]
-        //viewHolder.itemTitle.text = item.itemName
-        //viewHolder.itemDescription.text = item.itemDescription
+        viewHolder.myItemName.text = swap.myItem.name
+        viewHolder.otherItemName.text = swap.exchangeItem.name
 
         viewHolder.swapPopupMenu.setOnClickListener {
             showPopupMenu(viewHolder.swapPopupMenu, i)
@@ -113,7 +106,7 @@ class MySwapsAdapter(
         popup.show()
     }
 
-    private fun viewDetails(swap: Swap){
+    private fun viewDetails(swap: Matches.MatchesData.Match){
 
     }
 
