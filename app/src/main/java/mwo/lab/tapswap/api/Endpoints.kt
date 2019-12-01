ab72ad7839701f81c8bd7cc2ff3d1825d616c664
Kyriet <kyriet.sebastian@gmail.com>
2019-12-02 00:28:48 +0100

    Filling GUI with pending swaps

--- app/src/main/java/mwo/lab/tapswap/api/Endpoints.kt
+++ app/src/main/java/mwo/lab/tapswap/api/Endpoints.kt
@@ -47,12 +47,13 @@ interface Endpoints {
     @POST("/users/login")
     fun login(@Field("email") email: String, @Field("password") password: String) : Call<LoginResult>
 
+    @GET("matches/get-pending-matches")
+    fun getPendingMatches() : Call<Matches>
+
     @GET("/auth/istokenvalid")
     fun isTokenValid() : Call<RequestResult>
 
     @POST("/test")
     fun test() : Call<Request>
 
-    @GET("matches/get-accepted-matches")
-    fun getUserSwaps() : Call<UserSwaps>
 }
