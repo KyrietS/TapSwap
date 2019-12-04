7b598b2eecd692cebbe43c193ca6031e0aad9a49
Kyriet <kyriet.sebastian@gmail.com>
2019-12-04 12:16:24 +0100

    Change my account info

--- app/src/main/java/mwo/lab/tapswap/activities/MyAccountActivity.kt
+++ app/src/main/java/mwo/lab/tapswap/activities/MyAccountActivity.kt
@@ -1,7 +1,6 @@
 package mwo.lab.tapswap.activities
 
 import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
@@ -9,7 +8,9 @@ import android.view.View
 import android.widget.*
 import mwo.lab.tapswap.R
 import mwo.lab.tapswap.api.APIService
+import mwo.lab.tapswap.api.models.Myself
 import mwo.lab.tapswap.api.models.RequestResult
+import mwo.lab.tapswap.views.LoadingView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -25,6 +26,12 @@ class MyAccountActivity : AppCompatActivity() {
     private lateinit var saveChangesButton: Button
     private lateinit var progressBar: ProgressBar
     private lateinit var scrollView: ScrollView
+    private lateinit var loading: LoadingView
+
+    var password: String = ""
+    var name: String? = null
+    var phone: String? = null
+    var newPassword: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,61 +46,75 @@ class MyAccountActivity : AppCompatActivity() {
         saveChangesButton = findViewById(R.id.btn_signup)
         progressBar = findViewById(R.id.progress_bar)
         scrollView = findViewById(R.id.scroll_view)
+        loading = findViewById(R.id.loading)
         saveChangesButton.setOnClickListener { saveChanges() }
 
+        fillFormWithData()
+    }
+
+    private fun fillFormWithData() {
+        enableUI(false)
+        val api = APIService.create()
+        val call = api.getMyself()
+        loading.begin()
+        call.enqueue(object : Callback<Myself> {
+            override fun onResponse(call: Call<Myself>, response: Response<Myself>) {
+                loading.finish()
+                if(response.isSuccessful && response.body()?.success == true) {
+                    val myselfData = response.body()!!.data
+                    nameText.setText(myselfData.name)
+                    emailText.setText(myselfData.email)
+                    mobileText.setText(myselfData.phone)
+                    enableUI(true)
+                } else {
+                    onFailure(call, null)
+                }
+
+            }
+            override fun onFailure(call: Call<Myself>, t: Throwable?) {
+                enableUI(true)
+                Toast.makeText(this@MyAccountActivity, "Connection error", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        })
     }
 
     private fun saveChanges() {
         if (!validate()) {
-            onSignUpFailed()
+            onSaveFailed()
             return
         }
 
         // Scroll to the bottom to show the user progress bar
         scrollView.scrollTo(0, scrollView.bottom)
 
-        val name = nameText.text.toString()
-        val email = emailText.text.toString()
-        val mobile = mobileText.text.toString()
-        val old_password = oldPasswordText.text.toString()
-        val new_password = newPasswordText.text.toString()
+        name = nameText.text.toString()
+        phone = mobileText.text.toString()
+        password = oldPasswordText.text.toString()
 
 
         enableUI(false)
         progressBar.visibility = View.VISIBLE
 
-        //sendUptateAccountRequest(name, email, mobile, password)
+        sendUpdateAccountRequest(name, phone, password, newPassword)
     }
 
-    private fun sendUptateAccountRequest(
-        name: String,
-        email: String,
-        mobile: String,
+    private fun sendUpdateAccountRequest(
+        name: String?,
+        mobile: String?,
         old_password: String,
-        new_password: String
-
+        new_password: String?
     ) {
         val api = APIService.create()
-        val call = api.addUser(name, email, mobile, old_password)
+        val call = api.updateUser(name, mobile, old_password, new_password)
         call.enqueue(object : Callback<RequestResult> {
             override fun onResponse(call: Call<RequestResult>, response: Response<RequestResult>) {
-                val success = response.body()?.success ?: false
-                val errors = response.body()?.errors
-                if(response.isSuccessful && success) {
-                    onSignUpSuccess()
-                } else if (errors != null){
-                    Log.d("sign", "errors")
-                    Log.d("sign", response.raw().toString())
-                    errors.forEach {
-                        when(it.code) {
-                            "ER_DUP_ENTRY" -> emailText.error = "This email is already used"
-                        }
-                    }
-                    onSignUpFailed()
+                if(response.isSuccessful && response.body()?.success == true) {
+                    onSaveSuccess()
                 } else {
                     Log.d("sign", "unknown error")
                     Log.d("sign", response.raw().toString())
-                    onSignUpFailed()
+                    onSaveFailed()
                 }
             }
             override fun onFailure(call: Call<RequestResult>, t: Throwable) {
@@ -104,14 +125,15 @@ class MyAccountActivity : AppCompatActivity() {
         })
     }
 
-    private fun onSignUpSuccess() {
+    private fun onSaveSuccess() {
         Log.d("sign", "Success")
+        Toast.makeText(this, "Dane zostały zmienione", Toast.LENGTH_SHORT).show()
         setResult(Activity.RESULT_OK, null)
         finish()
     }
 
-    private fun onSignUpFailed() {
-        Toast.makeText(baseContext, "Sign up failed", Toast.LENGTH_SHORT).show()
+    private fun onSaveFailed() {
+        Toast.makeText(baseContext, "Wystąpił błąd", Toast.LENGTH_SHORT).show()
         enableUI(true)
         progressBar.visibility = View.GONE
     }
@@ -131,21 +153,18 @@ class MyAccountActivity : AppCompatActivity() {
             valid = false
         }
 
-        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            emailText.error = "enter a valid email address"
-            valid = false
-        }
-
         if (mobile.isEmpty() || mobile.length < 9 || mobile.contains("[^0-9 +-]".toRegex())) {
             mobileText.error = "Enter Valid Mobile Number"
             valid = false
         }
-        //if (password.isEmpty() || password.length < 4 || password.length > 10) {
-        //    passwordText.error = "between 4 and 10 alphanumeric characters"
-        //    valid = false
-        //}
 
-        if (reEnterPassword.isEmpty() || reEnterPassword.length < 4 || reEnterPassword.length > 10 || reEnterPassword != new_password) {
+        newPassword = if(new_password.isEmpty()) {
+            null
+        } else {
+            new_password
+        }
+
+        if (new_password.isNotEmpty() && reEnterPassword != new_password) {
             reEnterPasswordText.error = "Password Do not match"
             valid = false
         }
@@ -154,7 +173,7 @@ class MyAccountActivity : AppCompatActivity() {
     }
 
     private fun enableUI(state: Boolean) {
-        emailText.isEnabled = state
+        emailText.isEnabled = false
         oldPasswordText.isEnabled = state
         newPasswordText.isEnabled = state
         nameText.isEnabled = state
